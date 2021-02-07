e325c9c9976d8e72363732fee7c19d0a95d248bd
David Sterry <sterry.david@gmail.com>
2021-02-07 12:27:01 -0800

    add image to header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from "react";
-
+import ticketsImage from "./../img/ticket.png";
 function Header(){
   return (
+    <React.Fragment>
     <h1>Help Queue</h1>
+    <img src={ticketsImage} alt="An image of tickets" />
+    </React.Fragment>
   );
 }
 
