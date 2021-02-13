45e390f2f750a9d0c154ccaa862831aee3027df2
David Sterry <sterry.david@gmail.com>
2021-02-13 14:34:43 -0800

    update with last weekends hw changes

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,26 +1,27 @@
 import React from "react";
-import Ticket from "./Ticket";
 import PropTypes from "prop-types";
-
-
+import Ticket from "./Ticket";
 
 function TicketList(props){
   return (
     <React.Fragment>
-    <hr/>
-    {props.ticketList.map((ticket, index) =>
-      <Ticket names={ticket.names}
-        location={ticket.location}
-        issue={ticket.issue}
-        key={index}/>
-    )}
-  </React.Fragment>
+      <hr/>
+      {props.ticketList.map((ticket) =>
+        <Ticket
+          whenTicketClicked = { props.onTicketSelection }
+          names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          id={ticket.id}
+          key={ticket.id}/>
+      )}
+    </React.Fragment>
   );
 }
 
-//Add propTypes for ticketList
 TicketList.propTypes = {
-  ticketList: PropTypes.array
+  ticketList: PropTypes.array,
+  onTicketSelection: PropTypes.func
 };
 
 export default TicketList;
